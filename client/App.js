5f8f8587b5edc56330bc6a3a24ba26fe176c0b6e
Jase DiNardo <jaseiscoding@gmail.com>
2020-09-25 11:31:08 -0700

    Starting to initialize front end;
    Adding client files and dist dir with basic html page

---
+++ client/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+class App extends React.Component {
+  constructor(props) {
+
+    this.state = {
+      photos = [];
+      listing = {};
+    }
+  }
+
+
+
+
+  render() {
+    <div>
+      <p>this is where info goes</p>
+      <div>this is where photos go</div>
+    </div>
+  }
+}
