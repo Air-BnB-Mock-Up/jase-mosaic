e594f4f1eb20592cc5b58508d612e2e19cc34bba
Jase DiNardo <jaseiscoding@gmail.com>
2020-09-28 16:52:27 -0700

    Add style, transition, and animation to modal
    Rises to full screen with a delay in opacity and brightness

--- client/src/components/Index.jsx
+++ client/src/components/Index.jsx
@@ -8,11 +8,13 @@ import Modal from 'react-modal';
 // HELPER FUNCTIONS ////////////////////////
 import getInfo from '../httpHandler.js';
 import {photos, info} from '../store.js';
+import {ModalStyle} from '../styles/Modal.js';
 ////////////////////////////////////////////
 // REACT ELEMENTS //////////////////////////
 import Info from './info.jsx';
 import MosaicSmall from './mosaic_small.jsx';
 import PhotoFeed from './photo_feed.jsx';
+
 ////////////////////////////////////////////
 // MAIN PAGE ///////////////////////////////
 export class AppPhotos extends React.Component {
@@ -50,7 +52,7 @@ export class AppPhotos extends React.Component {
     return (
       <div>
         <header>Air BnB</header>
-        <Modal onRequestClose={this.clickHandle} isOpen={this.state.isOpen} style={{margin: '0', width: '100%'}}>
+        <Modal onRequestClose={this.clickHandle} isOpen={this.state.isOpen} style={ModalStyle} >
           <button onClick={this.clickHandle}>click me</button>
           <PhotoFeed photos={this.state.photos} />
         </Modal>
