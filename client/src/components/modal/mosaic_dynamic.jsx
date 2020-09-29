7205ea88fc47c9c6119047283f3d3d9098602bad
Jase DiNardo <jaseiscoding@gmail.com>
2020-09-29 15:52:06 -0700

    Will render modal of photos dynamically;

--- client/src/components/modal/mosaic_dynamic.jsx
+++ client/src/components/modal/mosaic_dynamic.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 export const ModalListItem = ({photos}) => {
   const [firstURL, ...otherTwo] = photos;
   return (
-    <div style={{border: 'thin solid black', width: '100%', height: '85vh', margin: '0'}}>
-      <div className="photo-large hover-box" style={{backgroundImage: `url(${firstURL})`, width: '100%', height: '62.7vh'}}></div>
+    <div style={{width: '100%', height: '85vh', margin: '1.1% 0'}}>
+      <div className="photo-large hover-box" style={{backgroundImage: `url(${firstURL})`, width: '100%', height: '62vh', marginBottom: '1%'}}></div>
       <div>
-        {otherTwo.map((url) => <div className="hover-box" style={{backgroundImage: `url(${url})`, width: '50%', height: '22vh', float: 'left'}} ></div> )}
+        {otherTwo.map((url, index) => <div id={`two-horizontal-${index}`} className="hover-box" style={{backgroundImage: `url(${url})`, width: '49.5%', height: '22vh', float: 'left', marginBottom: '1%'}} ></div> )}
       </div>
     </div>
   );
