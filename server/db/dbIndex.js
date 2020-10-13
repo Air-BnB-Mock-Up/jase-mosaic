b234f63af4b1bf15a0cee61b62a811c25b1816c0
Jase DiNardo <jaseiscoding@gmail.com>
2020-10-12 20:43:28 -0700

    quick

--- server/db/dbIndex.js
+++ server/db/dbIndex.js
@@ -1,5 +1,5 @@
 //////////////////////////////////////////////////////////
 const mongoose = require('mongoose');
-const db = mongoose.connect('mongodb://localhost/mosaic',
+const db = mongoose.connect('mongodb://mongodb://ec2-18-144-14-214.us-west-1.compute.amazonaws.com/mosaic',
 { useNewUrlParser: true, useUnifiedTopology: true });
 //////////////////////////////////////////////////////////
